9f7f2a126e981db1d0f04610caa60c76e71d03dc
panargirakis <panargirakis@gmail.com>
2023-07-04 14:31:23 -0400

    updated version

--- hwmux/provider.go
+++ hwmux/provider.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"os"
 
-	"github.com/Silabs-UTF/hwmux-client-golang"
+	"github.com/Silabs-UTF/hwmux-client-golang/v2"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
