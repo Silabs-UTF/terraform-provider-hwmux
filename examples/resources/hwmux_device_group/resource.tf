833b2adebeb3bcdd9ea36c1367c037c24143b34a
panargirakis <panargirakis@gmail.com>
2023-09-29 13:29:04 -0400

    updated docs

--- examples/resources/hwmux_device_group/resource.tf
+++ examples/resources/hwmux_device_group/resource.tf
@@ -1,7 +1,8 @@
 resource "hwmux_device_group" "new_testbed" {
-  name              = "name_no_spaces"
-  metadata          = jsonencode(yamldecode(file("example.yaml")))
-  devices           = [1, 2]
-  permission_groups = ["Example group name"]
-  enable_ahs        = true
+  name               = "name_no_spaces"
+  metadata           = jsonencode(yamldecode(file("example.yaml")))
+  devices            = [1, 2]
+  permission_groups  = ["Example group name"]
+  enable_ahs         = true
+  enable_ahs_actions = true
 }
