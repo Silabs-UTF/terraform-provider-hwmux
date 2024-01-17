9a960a2df44e415b237d4f2059b119e78faeecfa
panargirakis <panargirakis@gmail.com>
2024-01-17 13:03:19 -0500

    fixed naming

--- hwmux/devicegroup_data_source_test.go
+++ hwmux/devicegroup_data_source_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-var testResourceAttrName string = "data.hwmux_device_group.test"
+var deviceGroupDataSourceTfName string = "data.hwmux_device_group.test"
 
 func TestAccDeviceGroupDataSource(t *testing.T) {
 	resource.Test(t, resource.TestCase{
@@ -17,14 +17,14 @@ func TestAccDeviceGroupDataSource(t *testing.T) {
 				Config: providerConfig + `data "hwmux_device_group" "test" {id = 1}`,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					// Verify the first coffee to ensure all attributes are set
-					resource.TestCheckResourceAttr(testResourceAttrName, "name", "group0"),
-					resource.TestCheckResourceAttr(testResourceAttrName, "id", "1"),
+					resource.TestCheckResourceAttr(deviceGroupDataSourceTfName, "name", "group0"),
+					resource.TestCheckResourceAttr(deviceGroupDataSourceTfName, "id", "1"),
 					// ensure dynamic attributes are populated
-					resource.TestCheckResourceAttrSet(testResourceAttrName, "enable_ahs"),
-					resource.TestCheckResourceAttrSet(testResourceAttrName, "enable_ahs_actions"),
-					resource.TestCheckResourceAttrSet(testResourceAttrName, "enable_ahs_cas"),
-					resource.TestCheckResourceAttrSet(testResourceAttrName, "metadata"),
-					resource.TestCheckResourceAttrSet(testResourceAttrName, "devices.0.id"),
+					resource.TestCheckResourceAttrSet(deviceGroupDataSourceTfName, "enable_ahs"),
+					resource.TestCheckResourceAttrSet(deviceGroupDataSourceTfName, "enable_ahs_actions"),
+					resource.TestCheckResourceAttrSet(deviceGroupDataSourceTfName, "enable_ahs_cas"),
+					resource.TestCheckResourceAttrSet(deviceGroupDataSourceTfName, "metadata"),
+					resource.TestCheckResourceAttrSet(deviceGroupDataSourceTfName, "devices.0.id"),
 				),
 			},
 		},
--- hwmux/devicegroup_resource_test.go
+++ hwmux/devicegroup_resource_test.go
@@ -6,6 +6,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
+var deviceGroupResourceTfName string = "hwmux_device_group.test"
+
 func TestAccDeviceGroupResource(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
@@ -20,23 +22,23 @@ resource "hwmux_device_group" "test" {
 }
 `,
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(testResourceAttrName, "name", "test_dg"),
-					resource.TestCheckResourceAttr(testResourceAttrName, "devices.#", "2"),
-					resource.TestCheckResourceAttr(testResourceAttrName, "devices.0", "1"),
-					resource.TestCheckResourceAttr(testResourceAttrName, "permission_groups.#", "1"),
-					resource.TestCheckResourceAttr(testResourceAttrName, "permission_groups.0", "All users"),
+					resource.TestCheckResourceAttr(deviceGroupResourceTfName, "name", "test_dg"),
+					resource.TestCheckResourceAttr(deviceGroupResourceTfName, "devices.#", "2"),
+					resource.TestCheckResourceAttr(deviceGroupResourceTfName, "devices.0", "1"),
+					resource.TestCheckResourceAttr(deviceGroupResourceTfName, "permission_groups.#", "1"),
+					resource.TestCheckResourceAttr(deviceGroupResourceTfName, "permission_groups.0", "All users"),
 					// Verify the device_group item has Computed attributes filled.
-					resource.TestCheckResourceAttr(testResourceAttrName, "metadata", "{}"),
+					resource.TestCheckResourceAttr(deviceGroupResourceTfName, "metadata", "{}"),
 					// Verify dynamic values have any value set in the state.
-					resource.TestCheckResourceAttrSet(testResourceAttrName, "id"),
-					resource.TestCheckResourceAttrSet(testResourceAttrName, "enable_ahs"),
-					resource.TestCheckResourceAttrSet(testResourceAttrName, "enable_ahs_actions"),
-					resource.TestCheckResourceAttrSet(testResourceAttrName, "last_updated"),
+					resource.TestCheckResourceAttrSet(deviceGroupResourceTfName, "id"),
+					resource.TestCheckResourceAttrSet(deviceGroupResourceTfName, "enable_ahs"),
+					resource.TestCheckResourceAttrSet(deviceGroupResourceTfName, "enable_ahs_actions"),
+					resource.TestCheckResourceAttrSet(deviceGroupResourceTfName, "last_updated"),
 				),
 			},
 			// ImportState testing
 			{
-				ResourceName:      testResourceAttrName,
+				ResourceName:      deviceGroupResourceTfName,
 				ImportState:       true,
 				ImportStateVerify: true,
 				// The last_updated attribute does not exist in the HashiCups
@@ -56,16 +58,16 @@ resource "hwmux_device_group" "test" {
 }
 `,
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(testResourceAttrName, "name", "test_dg"),
-					resource.TestCheckResourceAttr(testResourceAttrName, "devices.#", "1"),
-					resource.TestCheckResourceAttr(testResourceAttrName, "devices.0", "1"),
-					resource.TestCheckResourceAttr(testResourceAttrName, "permission_groups.#", "1"),
-					resource.TestCheckResourceAttr(testResourceAttrName, "permission_groups.0", "Staff users"),
-					resource.TestCheckResourceAttr(testResourceAttrName, "enable_ahs", "true"),
-					resource.TestCheckResourceAttr(testResourceAttrName, "enable_ahs_actions", "true"),
-					resource.TestCheckResourceAttr(testResourceAttrName, "enable_ahs_cas", "true"),
+					resource.TestCheckResourceAttr(deviceGroupResourceTfName, "name", "test_dg"),
+					resource.TestCheckResourceAttr(deviceGroupResourceTfName, "devices.#", "1"),
+					resource.TestCheckResourceAttr(deviceGroupResourceTfName, "devices.0", "1"),
+					resource.TestCheckResourceAttr(deviceGroupResourceTfName, "permission_groups.#", "1"),
+					resource.TestCheckResourceAttr(deviceGroupResourceTfName, "permission_groups.0", "Staff users"),
+					resource.TestCheckResourceAttr(deviceGroupResourceTfName, "enable_ahs", "true"),
+					resource.TestCheckResourceAttr(deviceGroupResourceTfName, "enable_ahs_actions", "true"),
+					resource.TestCheckResourceAttr(deviceGroupResourceTfName, "enable_ahs_cas", "true"),
 					// Verify the device_group item has Computed attributes filled.
-					resource.TestCheckResourceAttr(testResourceAttrName, "metadata", "{}"),
+					resource.TestCheckResourceAttr(deviceGroupResourceTfName, "metadata", "{}"),
 				),
 			},
 			// Delete testing automatically occurs in TestCase
